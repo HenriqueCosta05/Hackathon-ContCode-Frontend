76466523a772e81b455e15b7f58e01a8855527ed
Clécio de Lira Alves <cclecioc.com@gmail.com>
2024-11-23 00:15:02 -0300

    html feito

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
 export default function Page() {
   return (
-      <main className="flex w-full h-screen justify-center items-center">
-        <h1 className="text-4xl font-bold text-teal-600">Hello World</h1>
-      </main>
-    )
+    <main className="flex flex-col w-full h-screen justify-center items-center">
+      <h1 className="text-4xl font-bold">ManuCar</h1>
+      <p className="pt-2 text-center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ab
+        impedit atque suscipit sequi sed nemo eaque esse.
+      </p>
+
+      <div className="flex space-x-5 pt-10">
+        <button className="p-3 w-36 rounded-md cadastrar">Cadastrar</button>
+
+        <button className="p-3 w-36 rounded-md login bg-transparent">
+          Entrar
+        </button>
+      </div>
+    </main>
+  );
 }
---
+++ src/app/register.tsx
@@ -0,0 +1,45 @@
+export default function Register() {
+  return (
+    <main className="flex flex-col w-full h-screen justify-center items-center">
+      <h1 className="text-4xl font-bold">Cadastre-se</h1>
+      <p className="pt-2 text-center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ab
+        impedit atque suscipit sequi sed nemo eaque esse.
+      </p>
+
+      <form className="flex flex-col space-y-5 pt-5 items-center">
+        <label className="space-x-3">
+          <span>Nome:</span>
+          <input
+            type="email"
+            placeholder="Digite o seu nome"
+            required
+            className="py-1 px-3 bg-transparent rounded-md placeholder-black w-64 inputform"
+          />
+        </label>
+
+        <label className="space-x-3">
+          <span>Email:</span>
+          <input
+            type="email"
+            placeholder="Digite o seu Email"
+            required
+            className="py-1 px-3 bg-transparent rounded-md placeholder-black w-64 inputform"
+          />
+        </label>
+
+        <label className="space-x-3">
+          <span>Senha:</span>
+          <input
+            type="email"
+            placeholder="Digite a sua senha"
+            required
+            className="py-1 px-3 bg-transparent rounded-md placeholder-black w-64 inputform"
+          />
+        </label>
+
+        <button className="cadastrar p-3 w-36 rounded-md">Cadastrar</button>
+      </form>
+    </main>
+  );
+}
