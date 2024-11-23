c07afee93226b0cf194bc11465780d7014b6416c
HenriqueCosta05 <Henrique.b.costa9090@gmail.com>
2024-11-22 22:15:38 -0300

    feat: initial setup of next

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
-      <main className="flex w-full h-screen justify-center items-center ">
-        
+      <main className="flex w-full h-screen justify-center items-center">
+        <h1 className="text-4xl font-bold text-teal-600">Hello World</h1>
       </main>
     )
 }
